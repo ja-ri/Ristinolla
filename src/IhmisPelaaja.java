b4618b6c8845732219969807c4e8203d726fad9c
vilho <vilho@kivihal.me>
2020-05-14 11:00:19 +0300

    tarvittavat muutokset kommenteissa

--- src/IhmisPelaaja.java
+++ src/IhmisPelaaja.java
@@ -1,11 +1,25 @@
 
 import java.util.Scanner;
 
-/*public class IhmisPelaaja extends Pelaaja {
+public class IhmisPelaaja extends Pelaaja {
 
-    @Override
-    public char[][] pelaaVuoro(char[][] pelilauta) {
+	@Override
+	public void pelaaVuoro(char[][] pelilauta) {
 
-        return new char[][];
-    }
-}*/
+		/**
+		 * Parametrina tulee pelilauta. Tämä luokka on ihmispelaaja.
+		 * 
+		 * Tämä luokka kysyy käyttäjän syötteen scannerilla, varmistaa, että se on
+		 * järkevä syöte ja kun järkevä syöte saadaan, pelimerkki asetetaan
+		 * pelilaudalle. Tarvitsette Scannerin, joten tehkää konstruktori, jossa annatte
+		 * sen parametrina. Jos tarvitaan myös Ristinolla-luokan metodeja, antakaa se myös Konstruktorin parametrina.
+		 * 
+		 * 
+		 * Tämä luokka ei tee MITÄÄN muuta, kuin pelaa yhden vuoron. Se ei tarkista
+		 * voittoja tai pelaa peliä, ainoa asia mitä pitäisi tapahtua, on oman
+		 * pelimerkin asettaminen laudalle.
+		 * 
+
+		 */
+	}
+}
